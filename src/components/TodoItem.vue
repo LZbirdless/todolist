73d477ea16bff3df146daa095297477ec32a11fd
nguyenvu59 <nguyenvu59@gmail.com>
2026-04-13 09:09:11 +0700

    test

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <li class="todo-item">
+    <span v-bind:class="{ completed: todo.completed }">{{ todo.text }}</span>
+    <button @click="deleteTodo(todo)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    deleteTodo(todo) {
+      this.$emit('delete-todo', todo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item.completed {
+  text-decoration: line-through;
+}
+</style>
